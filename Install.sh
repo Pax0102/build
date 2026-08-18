3b152f008f157e04d3b8d0c248dbf056026d9630
Pax <mamaetata216@gmail.com>
2026-08-17 21:51:07 -0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,1 +1,8 @@
-curl -L -o lynx-browser-linux-x64.tar.bz2 https://github.com/Pax0102/lynx-browser/releases/download/v1.0.28/lynx-browser-linux-x64.tar.bz2 && tar -xjf lynx-browser-linux-x64.tar.bz2 && cd LynxBrowser && chmod +x start.sh setup.sh lynx && ./start.sh
+#!/usr/bin/env bash
+set -euo pipefail
+
+curl -L -o lynx-browser-linux-x64.tar.bz2 https://github.com/Pax0102/lynx-browser/releases/download/v1.0.28/lynx-browser-linux-x64.tar.bz2
+tar -xjf lynx-browser-linux-x64.tar.bz2
+cd LynxBrowser
+chmod +x start.sh setup.sh lynx
+./start.sh
